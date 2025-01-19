25733a20ebf47cc57e6505dbc825b5774587dfa2
learningwithakshat <2004mittalakshat@gmail.com>
2025-01-20 00:07:32 +0530

    Img-Change

--- src/Pages/Left.jsx
+++ src/Pages/Left.jsx
@@ -4,7 +4,7 @@ const Left = () => {
   return (
     <div className="left hidden p-10 md:h-full w-full md:w-2/4 md:flex items-center justify-center">
     {/* <h1 className='text-3xl md:text-6xl font-bold font-Poppins text-center'>Welcome to <p className='font-Dancing text-2xl md:text-5xl mt-2'>The Community</p></h1> */}
-    <img className='h-full' src="./public/SE.png" alt="" />
+    <img className='./public/SE.png' src="./public/SE.png" alt="" />
     </div>
   )
 }
