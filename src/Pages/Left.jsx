3939b1a787f209d9648fa49514e5371ab92fb447
learningwithakshat <2004mittalakshat@gmail.com>
2025-01-20 00:33:40 +0530

    Update

--- src/Pages/Left.jsx
+++ src/Pages/Left.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Img from '../../assets/SE.png'
+import Img from '../../src/assets/SE.png'
 
 const Left = () => {
   return (
     <div className="left hidden p-10 md:h-full w-full md:w-2/4 md:flex items-center justify-center">
     {/* <h1 className='text-3xl md:text-6xl font-bold font-Poppins text-center'>Welcome to <p className='font-Dancing text-2xl md:text-5xl mt-2'>The Community</p></h1> */}
-    <img className={Img} src="./public/SE.png" alt="" />
+    <img className='h-full' src={Img} alt="" />
     </div>
   )
 }
